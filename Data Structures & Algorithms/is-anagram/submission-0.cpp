33eee7a5c16080b04c0458419694ca74d1a8238f
Harsha Gopisetti <75930250+CuriousLearner4@users.noreply.github.com>
2026-08-16 00:51:12 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int ch[26];
+        for(auto c:s){
+            ch[c-'a']++;
+        }
+
+        for(auto c:t){
+            ch[c-'a']--;
+        }
+
+        for(int i = 0 ; i < 26; ++i){
+            if(ch[i]!=0) return false;
+        }
+
+        return true;
+    }
+};
