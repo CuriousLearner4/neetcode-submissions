13ba57ad5f5844f25e8575cfdb2dddc1ee4d9117
Harsha Gopisetti <75930250+CuriousLearner4@users.noreply.github.com>
2026-08-16 11:42:02 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int productofarray = 1;
+        int countofzero = 0;
+        for(auto num : nums){
+            if(num==0){
+                countofzero++;
+                continue;
+            }
+            productofarray *=num;
+        }
+
+        vector<int> result;
+
+        for(auto num:nums){
+            if(countofzero>1){
+                result.push_back(0);
+            }else if(countofzero == 1){
+                if(num==0)
+                    result.push_back(productofarray);
+                else result.push_back(0);
+            }else{
+                result.push_back(productofarray/num);
+            }
+            
+        }
+        return result;
+    }
+};
