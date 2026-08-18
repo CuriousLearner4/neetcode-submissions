b82979cf8692f001ebbc3b51dc70e7ab6146b372
Harsha Gopisetti <75930250+CuriousLearner4@users.noreply.github.com>
2026-08-18 23:11:13 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    char tolowerchar(char& x){
+        if(x>='A' && x<='Z') return x+32;
+        return x;
+    }
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length()-1;
+
+        while(i<=j){
+            while(i<s.length() &&!(s[i]>='0' &&s[i]<='9')&& !(s[i]>='a'&&s[i]<='z') && !(s[i]>='A'&&s[i]<='Z')) i++;
+            while(j>=0 &&!(s[j]>='0' &&s[j]<='9')&& !(s[j]>='a'&&s[j]<='z') && !(s[j]>='A'&&s[j]<='Z')) j--;
+            if(tolowerchar(s[i])!=tolowerchar(s[j])){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        } 
+
+        return true;
+    }
+};
